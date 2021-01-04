1626193b13b5eb1b708312689ab8b2114e430f38
AJthebeast <75455189+AJthebeast@users.noreply.github.com>
2021-01-03 18:01:40 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+canvas{
+document.getElementById("mycanvas")
+ctx=canvas.getContext("2d")
+}
+ctx.beginPath();
+ctx.strokeStyle-"red";
+ctx.linewidth-"10";
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.arc(200,200,40,0,2* Math.PI);
